4d6ec7b6d0368eb6860489e671c3186fe0cdbf6c
Muhammad Imran <50030345+ilyasimran27@users.noreply.github.com>
2019-06-28 11:20:37 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MOBIDUNYA</title>
+<link rel="shortcut icon" href="i.ico">
+    <link rel="stylesheet" href="css/headstyle.css">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="mobile.png" alt="">
+            <span class="heading">MobiDunya</span>
+
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="Contact.html">Contact</a></li>
+                <li><a href="#">Order now</a></li>
+
+            </ul>
+        </nav>
+    </div>
+
+
+
+
+
+
+
+</header>
+<section class="ban">
+    <div class="overlay">
+        <div class="container">
+            <h1><span class="theme">MobileDunya</span>.com   </h1>
+        </div>
+        <button>Order Now</button>
+    </div>
+
+</section>
+<section class="boxes">
+
+    <div class="container">
+        <div class="box1">
+            <img src="mb3.png" >
+            <h2>MOTOROLA ULTRA MAX</h2>
+        <p>Display5.00-inch .Processor1.7GHz dual-core. Front Camera2-megapixel. Resolution720x1280 pixels .RAM2GB .OSAndroid 4.4 .Storage16GB .Rear Camera10-megapixel. </p>
+
+
+        </div>
+        <div class="box2">
+            <img src="mb9.png" >
+            <h2>MOTOROLA MINI</h2>
+            <p>Display4.30-inch.Processor1.7GHz dual-core.Front Camera2-megapixel.Resolution720x1280 pixels.RAM2GB.OSAndroid 4.2.Storage16GB.Rear Camera10-megapixel. </p>
+
+
+        </div>
+
+    </div>
+</section>
+<section class="ban2">
+    <div class="overlay">
+        <div class="container">
+            <h1><span class="theme">Be Conected</span></h1>
+        </div>
+
+    </div>
+
+</section>
+<section class="boxes2">
+
+    <div class="container">
+        <div class="box3">
+            <img src="mb20.jpg" >
+            <h2>SAMSUNG GALAXY</h2>
+            <p> Display5.00-inch.
+                Processorocta-core.
+                Front Camera2-megapixel.
+                Resolution1080x1920 pixels.
+                RAM2GB.
+                OSAndroid 4.2.
+                Storage16GB.
+                Rear Camera13-megapixel. </p>
+
+
+        </div>
+        <div class="box4">
+            <img src="mb12.jpg" >
+            <h2>GALAXY 1</h2>
+            <p>Display4.30-inch.
+                Processor1.2GHz dual-core.
+                Front Camera2-megapixel.
+                Resolution480x800 pixels.
+                RAM512MB.
+                OSAndroid 4.4.
+                Storage4GB.
+                Rear Camera5-megapixel. </p>
+
+
+        </div>
+
+    </div>
+</section>
+<section class="ban3">
+    <div class="overlay">
+        <div class="container">
+            <h1><span class="theme">Always be yours</span>   </h1>
+        </div>
+
+    </div>
+
+</section>
+<footer>
+    <div class="container">
+        <div class="bottomlayout">
+            <p> From Karachi Pakistan</p>
+            <p>&copy;All right received</p>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
